8df12137016a578add2732c132ae3ac7fef9978d
Merve Arpacıoğlu Türk <merveearp@icloud.com>
2025-03-18 23:28:06 +0300

    Add project files.

---
+++ C#EgitimKampi501/Repositories/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using C_EgitimKampi501.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_EgitimKampi501.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<List<ResultProductDto>> GetAllProductAsync();
+        Task CreateProductAsync(CreateProductDto createProductDto);
+        Task UpdateProductAsync(UpdateProductDto updateProductDto);
+        Task DeleteProductAsync(int id);
+        Task GetByProductAsync(int id);
+
+    }
+}
---
+++ C#EgitimKampi501/Repositories/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using C_EgitimKampi501.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_EgitimKampi501.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task CreateProductAsync(CreateProductDto createProductDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProductAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultProductDto>> GetAllProductAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetByProductAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProductAsync(UpdateProductDto updateProductDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
